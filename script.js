b1eeea4ff9787353c50da7052b2e8edadee077a7
DRodriguezArenas <144775859+DRodriguezArenas@users.noreply.github.com>
2023-10-20 13:45:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const guardar = document.getElementById("guardarDatos");
+const mostrar = document.getElementById("mostrarDatos");
+
+let persona = {};
+
+guardar.addEventListener("click", () => {
+
+    let nombre = prompt("Ingrese su nombre:");
+    let apellido = prompt("Ingrese su apellido:");
+    let edad = prompt("Ingrese su edad:");
+
+    persona.nombre = nombre;
+    persona.apellido = apellido;
+    persona.edad = edad;
+    })
+
+persona.mostrarInfo = function() {
+  let div = document.getElementById("datos");
+  div.innerHTML = "Nombre: " + this.nombre + "<br>";
+  div.innerHTML += "Apellido: " + this.apellido + "<br>";
+  div.innerHTML += "Edad: " + this.edad + "<br>";
+}
+
+mostrar.addEventListener("click", function() {
+  persona.mostrarInfo();
+});
